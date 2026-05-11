a9c2c8a834129d3c7aa30752d6a110ae266f83a6
pshan <pshan0120@naver.com>
2026-05-11 18:41:34 +0900

    작품 개선

---
+++ 바스타즈 오브 더 유니버스/texts/replace.ps1
@@ -0,0 +1,9 @@
+﻿$files = Get-ChildItem '*.txt'
+foreach ($f in $files) {
+    $content = [System.IO.File]::ReadAllText($f.FullName)
+    if ($content -match '<span class="character-bastards">바스타즈</span>') {
+        $content = $content.Replace('<span class="character-bastards">바스타즈</span>', '<span class="character-bastards">바스타즈</span>')
+        [System.IO.File]::WriteAllText($f.FullName, $content, [System.Text.Encoding]::UTF8)
+        Write-Output "Updated $($f.Name)"
+    }
+}
